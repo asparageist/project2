383e538aabcdfc4dc3f47cc6944b8d41f5e5ab7d
Joseph Murray <josephwmurray@gmail.com>
2023-06-17 23:29:32 -0700

    apply the functionality of the Eyes option, apply reset buttons

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,21 @@ window.onload = function () {
     event.preventDefault();
     let showResult = document.getElementById("output");
     showResult.removeAttribute("class", "hidden");
+
+    const eyes = document.getElementById("eyes").value;
+    const arms = document.getElementById("arms").value;
+    const human = parseInt(document.getElementById("human").value);
+    const murder = document.getElementById("murder").value;
+    const grasp = document.getElementById("grasp").value;
+
+    if (eyes === "Yes") {
+      let showResult = document.getElementById("hidePage");
+      showResult.removeAttribute("class", "hidden");
+      } else if (eyes === "No") {
+      let showResult = document.getElementById("hidePage");
+      showResult.setAttribute("class", "hidden");  
+      let showButton = document.getElementById("wellThen");
+      showButton.removeAttribute("class", "hidden");
+    }
   }
 }
